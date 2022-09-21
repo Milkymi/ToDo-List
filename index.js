b3833a12ddab8ae09a0b8d757375d4590d900386
Milana <mrdv.milky@mail.ru>
2022-09-21 15:32:21 +0300

    release 3

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ let  list__button__white = className = 'list__button__white';
 btn.addEventListener('click', () => {
     const btnDone = document.createElement('button');
     const item = document.createElement('div');
+    item.style.cssText = `padding: 10px;`;
     btnDone.classList.add(list__button, list__button__done);
-    btnDone.textContent = '✓';
-    const span = document.createElement('div');
+    btnDone.innerHTML = `<input type="checkbox">`;
+    const span = document.createElement('span');
+    span.style.cssText = `margin: 3px;`;
     item.className = "list__item";
     const btnWhite = document.createElement('button');
     btnWhite.classList.add(list__button, list__button__white);
@@ -24,22 +26,15 @@ btn.addEventListener('click', () => {
     item.prepend(btnDone, span, btnWhite);
     list__container.prepend(item);
     input.value = '';
-
-
+    
     
     btnWhite.addEventListener('click', function (event) {
         const remove = event.target.parentNode.remove();
-    })
+    });
+
+    btnDone.addEventListener('click', function () {
+        span.classList.toggle('strike');
+    });
 });
 
 
-
-/* Release 2
-Добавь к кнопке сразу после создания обработчик события. Обработчик должен удалять нужный элемент списка дел.
-
-В данном релизе тебе поможет знание навигации по DOM-дереву.
-
-Алгоритм удаления элемента будет примерно таким:
-
-узнать на какую именно кнопку кликнули. Для этого в обработчике нужно принять объект события и обратиться к его свойству target.
-подняться от узла кнопки до его родителя и удалить его. */
